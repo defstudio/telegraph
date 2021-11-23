59c5742cb8eb26120dbe0866a0abbd55f550be62
Fabio Ivona <fabio.ivona@defstudio.it>
2021-11-23 11:36:37 +0100

    fix factories

--- database/factories/TelegraphChatFactory.php
+++ database/factories/TelegraphChatFactory.php
@@ -18,7 +18,7 @@ class TelegraphChatFactory extends Factory
         return [
             'chat_id' => $this->faker->randomNumber(),
             'name' => $this->faker->word,
-            'telegraph_bot_id' => fn() => TelegraphBot::factory()->create(),
+            'telegraph_bot_id' => fn () => app(TelegraphBotFactory::class)->create(),
         ];
     }
 }
