105b14a82fc93fba45c94db198106331ea35e882
fabio-ivona <fabio-ivona@users.noreply.github.com>
2021-12-06 20:44:55 +0000

    deploy: 071c300fff0a0efb95620d7347866ff8d237336c

--- _nuxt/static/1638823174/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Freleases","\u002Finstallation","\u002Fabout-us","\u002Fissues","\u002Ftelegram-bots\u002Fregister-new-chat","\u002Ftelegram-bots\u002Fregister-new-bot","\u002Ftelegram-bots\u002Fnew-bot","\u002Ftelegram-bots\u002Fsetting-webhook","\u002Fsupport-us"]})
