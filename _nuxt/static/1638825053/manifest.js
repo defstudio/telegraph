aa596f51cd0c02430a4fafb49e6e1955d86db874
fabio-ivona <fabio-ivona@users.noreply.github.com>
2021-12-09 07:37:54 +0000

    deploy: ce56acfee97f5feb70cae69721bae9b5a2894f02

--- _nuxt/static/1638825053/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Freleases","\u002Fwebhooks\u002Foverview","\u002Fabout-us","\u002Fissues","\u002Finstallation","\u002Fsupport-us","\u002Fquickstart\u002Fregister-new-chat","\u002Fquickstart\u002Fnew-bot","\u002Fquickstart\u002Fregister-new-bot","\u002Fquickstart\u002Fsetting-webhook"]})
---
+++ _nuxt/static/1639035472/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002Freleases","\u002F","\u002Fquickstart\u002Fregister-new-chat","\u002Finstallation","\u002Fissues","\u002Fquickstart\u002Fsetting-webhook","\u002Fabout-us","\u002Fquickstart\u002Fregister-new-bot","\u002Fsupport-us","\u002Fwebhooks\u002Foverview","\u002Fquickstart\u002Fnew-bot"]})
