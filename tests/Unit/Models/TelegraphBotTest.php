e18e9dfa3ae4830ca273bfc8883110abfc52799c
Fabio Ivona <fabio.ivona@defstudio.it>
2022-02-27 22:45:33 +0100

    improved tests

---
+++ tests/Unit/Models/TelegraphBotTest.php
@@ -0,0 +1,40 @@
+<?php
+
+/** @noinspection PhpUnhandledExceptionInspection */
+
+use DefStudio\Telegraph\Facades\Telegraph;
+use function Spatie\Snapshots\assertMatchesSnapshot;
+
+it('can retrieve its telegram info', function () {
+    Telegraph::fake();
+    $bot = make_bot();
+
+    assertMatchesSnapshot($bot->info());
+});
+
+it('can register its webhook', function () {
+    Telegraph::fake();
+    $bot = make_bot();
+
+    $bot->registerWebhook()->send();
+
+    Telegraph::assertRegisteredWebhook();
+});
+
+it('can get its webhook debug info', function () {
+    Telegraph::fake();
+    $bot = make_bot();
+
+    $bot->getWebhookDebugInfo()->send();
+
+    Telegraph::assertRequestedWebhookDebugInfo();
+});
+
+it('can reply a webhook call', function () {
+    Telegraph::fake();
+    $bot = make_bot();
+
+    $bot->replyWebhook(1231456, 'hello')->send();
+
+    Telegraph::assertRepliedWebhook('hello');
+});
