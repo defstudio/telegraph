ef89ea165a2a78104a4484fc20a12b843d44df02
Fabio Ivona <fabio.ivona@defstudio.it>
2022-03-01 17:57:08 +0100

    add analytics to documentation

--- docs/nuxt.config.js
+++ docs/nuxt.config.js
@@ -1,14 +1,17 @@
 import theme from '@nuxt/content-theme-docs'
 
 export default theme({
-  docs: {
-    primaryColor: '#1093ff'
-  },
-  target: 'static',
-  content: {
-    liveEdit: false
-  },
-  router: {
-    base: '/telegraph'
-  }
-})
+    docs: {
+        primaryColor: '#1093ff'
+    },
+    target: 'static',
+    content: {
+        liveEdit: false
+    },
+    router: {
+        base: '/telegraph'
+    },
+    buildModules: [
+        ['@nuxtjs/google-analytics', {id: 'G-V0R3GDCRFP'}]
+    ]
+});
