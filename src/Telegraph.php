27e26946d5976497665815b8ffb3661255a9598d
AmirHosein Rahimi <amirhoseinrahimi.work@gmail.com>
2022-08-18 01:43:18 +0430

    add when method

--- src/Telegraph.php
+++ src/Telegraph.php
@@ -85,6 +85,15 @@ class Telegraph
         $this->files = Collection::empty();
     }
 
+    public function when(bool $condition, callable $callback): Telegraph
+    {
+        if ($condition) {
+            return $callback($this);
+        }
+
+        return $this;
+    }
+
     public function send(): TelegraphResponse
     {
         $response = $this->sendRequestToTelegram();
