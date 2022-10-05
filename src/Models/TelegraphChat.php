8f796898d86a29b49115a59f196652e35735d7d6
Fabio Ivona <fabio.ivona@defstudio.it>
2022-10-05 12:24:51 +0200

    Update src/Models/TelegraphChat.php

--- src/Models/TelegraphChat.php
+++ src/Models/TelegraphChat.php
@@ -272,8 +272,8 @@ class TelegraphChat extends Model
         return TelegraphFacade::chat($this)->quiz($question);
     }
 
-    public function forwardMessage(TelegraphChat|int $fromChatId, int $messageId): Telegraph
+    public function forwardMessage(TelegraphChat|int $fromChat, int $messageId): Telegraph
     {
-        return TelegraphFacade::chat($this)->forwardMessage($fromChatId, $messageId);
+        return TelegraphFacade::chat($this)->forwardMessage($fromChat, $messageId);
     }
 }
