f5ccc55b4eeba4d640ef7e6c2f1bcf1d94fb0295
mario <mario.gattolla@gmail.com>
2022-10-06 10:37:19 +0200

    new test for InlineQuery Document class

---
+++ tests/Unit/DTO/InlineQueryResultDocumentTest.php
@@ -0,0 +1,26 @@
+<?php
+
+use DefStudio\Telegraph\DTO\InlineQueryResultDocument;
+
+it('can export to array', function () {
+    expect(
+        InlineQueryResultDocument::make('a45', 'testTitle', 'testDocumentUrl', 'testMimeType')
+            ->caption('testCaption')
+            ->description('testDescription')
+            ->thumbUrl('testThumbUrl')
+            ->thumbWidth(400)
+            ->thumbHeight(200)
+            ->toArray()
+    )->toBe([
+        '$title' => 'testTitle',
+        '$document_url' => 'testDocumentUrl',
+        '$mime_type' => 'testMimeType',
+        '$caption' => 'testCaption',
+        '$description' => 'testDescription',
+        '$thumb_url' => 'testThumbUrl',
+        '$thumb_width' => 400,
+        '$thumb_height' => 200,
+        'id' => "a45",
+        'type' => "document",
+    ]);
+});
