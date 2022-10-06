822edbd0e757378c6c997ba04a0e754d5490b884
mario <mario.gattolla@gmail.com>
2022-10-06 09:35:54 +0200

    new test for InlineQueryVoice class

---
+++ tests/Unit/DTO/InlineQueryResultVoiceTest.php
@@ -0,0 +1,19 @@
+<?php
+
+use DefStudio\Telegraph\DTO\InlineQueryResultVoice;
+
+it('can export to array', function () {
+    expect(
+        InlineQueryResultVoice::make('a45', 'testVoiceUrl', 'testTitle')
+        ->caption('testCaption')
+        ->duration(10)
+        ->toArray()
+    )->toBe([
+        '$voice_url' => 'testVoiceUrl',
+        '$title' => 'testTitle',
+        '$caption' => 'testCaption',
+        '$voice_duration' => 10,
+        'id' => "a45",
+        'type' => "voice",
+    ]);
+});
