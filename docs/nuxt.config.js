b5de4feca2f682874146c2fd6958bbb6b668c3aa
Fabio Ivona <fabio.ivona@defstudio.it>
2023-08-12 22:29:17 +0200

    fix docs

--- docs/nuxt.config.js
+++ docs/nuxt.config.js
@@ -1,21 +1,7 @@
 import theme from '@nuxt/content-theme-docs'
 
 export default theme({
-    docs: {
-        primaryColor: '#1093ff'
-    },
-    target: 'static',
-    content: {
-        liveEdit: false
-    },
-    router: {
-        base: '/telegraph'
-    },
-    build: {
-        optimization: {
-            splitChunks: {
-                name: true
-            }
-        }
-    }
-});
+  docs: {
+    primaryColor: '#E24F55'
+  }
+})
