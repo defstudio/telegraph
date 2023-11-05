563434b13aafd8e4cdfe700b4339a0d338bae87d
varemel <varemel@mail.ru>
2023-11-05 15:16:15 +0300

    refactor(test): FileStoreDriverTest to pass tests in Windows (forbidden letters in folder name)
    Anonymous class has static::class = ".../FileStoreDriverTest.php:14$49". Char ":" is forbidden in folder names in Windows.

---
+++ tests/Unit/Storage/TestStorable.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace DefStudio\Telegraph\Tests\Unit\Storage;
+
+use DefStudio\Telegraph\Concerns\HasStorage;
+use DefStudio\Telegraph\Contracts\Storable;
+
+class TestStorable implements Storable {
+    use HasStorage;
+
+    public function storageKey(): string
+    {
+        return 'foo';
+    }
+}
