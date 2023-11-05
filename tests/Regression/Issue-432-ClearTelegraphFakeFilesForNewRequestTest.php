832002449caec52467c648848bc807919ecb3a31
varemel <varemel@mail.ru>
2023-11-05 15:13:03 +0300

    fix(fake): telegraph fake attached files not clearing between fake requests

---
+++ tests/Regression/Issue-432-ClearTelegraphFakeFilesForNewRequestTest.php
@@ -0,0 +1,13 @@
+<?php
+
+use DefStudio\Telegraph\Facades\Telegraph as TelegraphFacade;
+
+test('Clear Telegraph Fake attached files for every new request', function() {
+    TelegraphFacade::fake();
+    $chat = make_chat();
+    $chat->photo(Storage::path('photo.jpg'))->message('test');
+    $telegraphB = $chat->message('123');
+    $filesB = $telegraphB->toArray()['files'];
+
+    expect($filesB)->toBeEmpty();
+});
