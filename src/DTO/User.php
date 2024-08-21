997bf271d2f4c78a7adb745082d5a977b5b9923d
AmirHosein Rahimi <amirhoseinrahimi.work@gmail.com>
2024-08-21 18:40:13 +0330

    Update User.php

--- src/DTO/User.php
+++ src/DTO/User.php
@@ -25,7 +25,7 @@ class User implements Arrayable, Storable
     }
 
     /**
-     * @param array{id:int, is_bot?:bool, first_name:string, last_name?:string, username?:string, language_code?:string, is_premium?:bool} $data
+     * @param array{id:int, is_bot?:bool, first_name?:string, last_name?:string, username?:string, language_code?:string, is_premium?:bool} $data
      */
     public static function fromArray(array $data): User
     {
@@ -34,7 +34,7 @@ class User implements Arrayable, Storable
         $user->id = $data['id'];
         $user->isBot = $data['is_bot'] ?? false;
 
-        $user->firstName = $data['first_name'];
+        $user->firstName = $data['first_name'] ?? '';
         $user->lastName = $data['last_name'] ?? '';
         $user->username = $data['username'] ?? '';
         $user->languageCode = $data['language_code'] ?? '';
