4f1707e817400abaaa35585dee9fced71c2fd0fc
Andrey Helldar <helldar@dragon-code.pro>
2024-09-12 10:54:13 +0300

    Make protected methods

--- src/Handlers/WebhookHandler.php
+++ src/Handlers/WebhookHandler.php
@@ -51,7 +51,7 @@ abstract class WebhookHandler
         $this->originalKeyboard = Keyboard::make();
     }
 
-    private function handleCallbackQuery(): void
+    protected function handleCallbackQuery(): void
     {
         $this->extractCallbackQueryData();
 
@@ -73,7 +73,7 @@ abstract class WebhookHandler
         App::call([$this, $action], $this->data->toArray());
     }
 
-    private function handleCommand(Stringable $text): void
+    protected function handleCommand(Stringable $text): void
     {
         $command = (string) $text->after('/')->before(' ')->before('@');
         $parameter = (string) $text->after('@')->after(' ');
@@ -100,7 +100,7 @@ abstract class WebhookHandler
         }
     }
 
-    private function handleMessage(): void
+    protected function handleMessage(): void
     {
         $this->extractMessageData();
 
