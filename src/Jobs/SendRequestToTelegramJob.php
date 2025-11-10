937e06b7b15098d4f2c79ebb8d9e43d5560fdf2a
AmirHosein Rahimi <amirhoseinrahimi.work@gmail.com>
2025-11-10 17:23:54 +0330

    feat: Proxy for requests in job

--- src/Jobs/SendRequestToTelegramJob.php
+++ src/Jobs/SendRequestToTelegramJob.php
@@ -42,6 +42,11 @@ class SendRequestToTelegramJob implements ShouldQueue
             $request
         );
 
+        // Apply proxy configuration if set
+        if ($proxy = config('telegraph.http_proxy')) {
+            $request->withOptions(['proxy' => $proxy]);
+        }
+
         /** @phpstan-ignore-next-line  */
         $request->timeout(config('telegraph.http_timeout', 30))->connectTimeout(config('telegraph.http_connection_timeout', 10))->post($this->url, $this->data);
     }
